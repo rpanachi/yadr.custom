63c228c22ea6830f98d6a707584d252d48b1207a
Rodrigo Panachi <rodrigopanachi@gmail.com>
2014-05-02 13:53:45 -0300

    Updating aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,11 @@
+# bin
 alias grunt="node_modules/grunt-cli/bin/grunt"
 alias redis.server="redis-server /usr/local/etc/redis.conf --daemonize yes"
-alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
 alias rm="rm -v $1"
+
+# utils
+alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+alias myip="curl ifconfig.me"
+
+# stuff
 alias router="ssh root@192.168.1.1"

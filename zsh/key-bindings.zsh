006834a191d77957b90b109ee2c52e69e37c10e4
Rodrigo Panachi <rodrigopanachi@gmail.com>
2014-05-02 14:14:15 -0300

    More key bindings

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -1,4 +1,7 @@
 # Key Bindings
 bindkey "^[[H" beginning-of-line # home
 bindkey "^[[F" end-of-line       # end
+bindkey "^[[1;9D" backward-word  # alt + left
+bindkey "^[[1;9C" forward-word   # alt + right
+
 bindkey '^r' history-incremental-search-backward

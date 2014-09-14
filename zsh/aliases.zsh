849a3649bfd943e533b0f802e01576bc076e14ef
Rodrigo Panachi <rpanachi@gmail.com>
2014-09-14 20:57:46 -0300

    More aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,11 @@
 # bin
 alias grunt="node_modules/grunt-cli/bin/grunt"
-alias redis.server="redis-server /usr/local/etc/redis.conf --daemonize yes && echo 'Redis Server started successfully!'"
+alias redis.server="redis-server /usr/local/etc/redis.conf --daemonize yes"
+alias postgres.server="pg_ctl -D /usr/local/var/postgres"
 alias rm="rm -iv $1"
+
+# dev
+alias be="bundle exec"
 
 # utils
 alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"

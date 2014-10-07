878c4ec24c274afd9ceaed5bb2a4cb8deda1eff1
Rodrigo Panachi <rpanachi@gmail.com>
2014-10-07 20:36:43 -0300

    Removing autojump from install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ rm -v $HOME/.editrc
 # zsh
 rm -rfv $HOME/.yadr/zsh/key-bindings.zsh
 ln -snfv $YADR/zsh/aliases.zsh $HOME/.yadr/zsh/aliases.custom.zsh
-ln -snfv $YADR/zsh/autojump.zsh $HOME/.yadr/zsh/autojump.custom.zsh
 ln -snfv $YADR/zsh/rbenv.zsh $HOME/.yadr/zsh/rbenv.custom.zsh
 ln -snfv $YADR/zsh/key-bindings.zsh $HOME/.yadr/zsh/zzzz_key-bindings.zsh
 

c9fbfe97e3ed4041017794a6d00d8bb06975f5d5
Rodrigo Panachi <rpanachi@gmail.com>
2015-02-04 09:50:10 -0200

    Fixing git configs

--- install.sh
+++ install.sh
@@ -18,6 +18,8 @@ rm -rfv $HOME/.vim/settings/disable-arrowkeys.vim
 ln -snfv $YADR/vim/guifont.vim $HOME/.vim/settings/guifont.custom.vim
 ln -snfv $YADR/vim/syntastic.vim $HOME/.vim/settings/syntastic.custom.vim
 ln -snfv $YADR/vim/guifont.vim $HOME/.vim/settings/zzzz_guifont.custom.vim
+ln -snfv $YADR/vim/vimrc.before $HOME/.vimrc.before
+ln -snfv $YADR/vim/vimrc.after $HOME/.vimrc.after
 
 # git
 ln -snfv $YADR/git/gitconfig $HOME/.gitconfig.user
@@ -25,7 +27,7 @@ read -p "Enter your git name: " NAME
 read -p "Enter yout git email: " EMAIL
 if [[ -n "$NAME" ]] && [[ -n "$EMAIL" ]]
 then
-  git config --global user.name $NAME
-  git config --global user.email $EMAIL
+  git config --global user.name "$NAME"
+  git config --global user.email "$EMAIL"
   echo "Git credentials set to $NAME <$EMAIL>"
 fi
